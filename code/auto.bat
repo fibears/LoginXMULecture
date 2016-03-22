0ab33b8af5a798980f230b9aa562d72de5993bf7
fibears <fibears33@sina.com>
2016-03-22 11:36:10 +0800

    update
    update auto.bat file

--- code/auto.bat
+++ code/auto.bat
@@ -1,7 +1,8 @@
 @REM @Author: fibears
 @REM @Date:   2016-03-21 09:38:49
 @REM @Last Modified by:   fibears
-@REM Modified time: 2016-03-21 15:04:59
+@REM Modified time: 2016-03-22 11:35:49
 
 python getcookie.py Username Passwd
 python GrabLecture.py
+cmd.exe
