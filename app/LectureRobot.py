76e68e7acc31677e37735e14d23e9e65c5770657
fibears <fibears33@sina.com>
2016-09-16 19:56:49 +0800

    update

--- app/LectureRobot.py
+++ app/LectureRobot.py
@@ -9,6 +9,7 @@ import cookielib
 import random
 import lxml
 import re
+import webbrowser
 
 from lxml.html import parse
 from agents import AGENTS
@@ -37,13 +38,14 @@ class Ui_Widget(object):
         self.headers = {'User-Agent': self.user_agent}
         # Default URL
         self.LectureUrl = 'http://event.wisesoe.com/LectureOrder.aspx'
+        self.cookie = cookielib.MozillaCookieJar()
 
     def setupUi(self, Widget):
         Widget.setObjectName(_fromUtf8("Widget"))
-        Widget.resize(529, 300)
+        Widget.resize(529, 341)
         Widget.setAutoFillBackground(True)
         self.verticalLayoutWidget_2 = QtGui.QWidget(Widget)
-        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(220, 60, 301, 221))
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(220, 60, 301, 251))
         self.verticalLayoutWidget_2.setObjectName(_fromUtf8("verticalLayoutWidget_2"))
         self.verticalLayout_2 = QtGui.QVBoxLayout(self.verticalLayoutWidget_2)
         self.verticalLayout_2.setMargin(11)
@@ -67,16 +69,33 @@ class Ui_Widget(object):
         self.label_6.setObjectName(_fromUtf8("label_6"))
         self.verticalLayout_3.addWidget(self.label_6)
         self.gridLayoutWidget = QtGui.QWidget(Widget)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(10, 60, 211, 221))
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(10, 60, 211, 251))
         self.gridLayoutWidget.setObjectName(_fromUtf8("gridLayoutWidget"))
         self.gridLayout_2 = QtGui.QGridLayout(self.gridLayoutWidget)
         self.gridLayout_2.setMargin(11)
         self.gridLayout_2.setSpacing(6)
         self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.commandLinkButton_3 = QtGui.QCommandLinkButton(self.gridLayoutWidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.commandLinkButton_3.sizePolicy().hasHeightForWidth())
+        self.commandLinkButton_3.setSizePolicy(sizePolicy)
+        self.commandLinkButton_3.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
+        self.commandLinkButton_3.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.commandLinkButton_3.setIconSize(QtCore.QSize(16, 16))
+        self.commandLinkButton_3.setAutoDefault(False)
+        self.commandLinkButton_3.setDefault(False)
+        self.commandLinkButton_3.setObjectName(_fromUtf8("commandLinkButton_3"))
+        self.gridLayout_2.addWidget(self.commandLinkButton_3, 6, 1, 1, 1)
+        # Author Button #
+        self.commandLinkButton_3.clicked.connect(self.Author)
         self.label_2 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.gridLayout_2.addWidget(self.label_2, 1, 0, 1, 1)
         self.label = QtGui.QLabel(self.gridLayoutWidget)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName(_fromUtf8("label"))
         self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1)
         self.commandLinkButton = QtGui.QCommandLinkButton(self.gridLayoutWidget)
@@ -91,8 +110,11 @@ class Ui_Widget(object):
         self.commandLinkButton.setAutoDefault(False)
         self.commandLinkButton.setDefault(False)
         self.commandLinkButton.setObjectName(_fromUtf8("commandLinkButton"))
-        self.gridLayout_2.addWidget(self.commandLinkButton, 3, 1, 1, 1)
+        self.gridLayout_2.addWidget(self.commandLinkButton, 4, 1, 1, 1)
+        # DropLink Button #
+        self.commandLinkButton.clicked.connect(self.DropLink)
         self.label_3 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
         self.label_3.setObjectName(_fromUtf8("label_3"))
         self.gridLayout_2.addWidget(self.label_3, 2, 0, 1, 1)
         self.commandLinkButton_2 = QtGui.QCommandLinkButton(self.gridLayoutWidget)
@@ -107,13 +129,16 @@ class Ui_Widget(object):
         self.commandLinkButton_2.setAutoDefault(False)
         self.commandLinkButton_2.setDefault(False)
         self.commandLinkButton_2.setObjectName(_fromUtf8("commandLinkButton_2"))
-        self.gridLayout_2.addWidget(self.commandLinkButton_2, 4, 1, 1, 1)
+        self.gridLayout_2.addWidget(self.commandLinkButton_2, 5, 1, 1, 1)
+        # MoreDetails Button #
+        self.commandLinkButton_2.clicked.connect(self.MoreDetails)
         self.lineEdit = QtGui.QLineEdit(self.gridLayoutWidget)
         self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
         self.gridLayout_2.addWidget(self.lineEdit, 0, 1, 1, 1)
         self.label_4 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
         self.label_4.setObjectName(_fromUtf8("label_4"))
-        self.gridLayout_2.addWidget(self.label_4, 3, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.label_4, 4, 0, 1, 1)
         self.lineEdit_2 = QtGui.QLineEdit(self.gridLayoutWidget)
         self.lineEdit_2.setInputMask(_fromUtf8(""))
         self.lineEdit_2.setEchoMode(QtGui.QLineEdit.Password)
@@ -127,27 +152,30 @@ class Ui_Widget(object):
         self.pushButton.setSizePolicy(sizePolicy)
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
         self.gridLayout_2.addWidget(self.pushButton, 2, 1, 1, 1)
-        # add #
-        self.pushButton.clicked.connect(self.Selected)
+        # GetCookie Button #
+        self.pushButton.clicked.connect(self.GetCookies)
         self.label_5 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_5.setAlignment(QtCore.Qt.AlignCenter)
         self.label_5.setObjectName(_fromUtf8("label_5"))
-        self.gridLayout_2.addWidget(self.label_5, 4, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.label_5, 5, 0, 1, 1)
         self.label_7 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_7.setAlignment(QtCore.Qt.AlignCenter)
         self.label_7.setObjectName(_fromUtf8("label_7"))
-        self.gridLayout_2.addWidget(self.label_7, 5, 0, 1, 1)
-        self.commandLinkButton_3 = QtGui.QCommandLinkButton(self.gridLayoutWidget)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
+        self.gridLayout_2.addWidget(self.label_7, 6, 0, 1, 1)
+        self.label_8 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_8.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_8.setObjectName(_fromUtf8("label_8"))
+        self.gridLayout_2.addWidget(self.label_8, 3, 0, 1, 1)
+        self.pushButton_2 = QtGui.QPushButton(self.gridLayoutWidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.commandLinkButton_3.sizePolicy().hasHeightForWidth())
-        self.commandLinkButton_3.setSizePolicy(sizePolicy)
-        self.commandLinkButton_3.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
-        self.commandLinkButton_3.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.commandLinkButton_3.setIconSize(QtCore.QSize(16, 16))
-        self.commandLinkButton_3.setAutoDefault(False)
-        self.commandLinkButton_3.setDefault(False)
-        self.commandLinkButton_3.setObjectName(_fromUtf8("commandLinkButton_3"))
-        self.gridLayout_2.addWidget(self.commandLinkButton_3, 5, 1, 1, 1)
+        sizePolicy.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
+        self.pushButton_2.setSizePolicy(sizePolicy)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.gridLayout_2.addWidget(self.pushButton_2, 3, 1, 1, 1)
+        # GrabLecture Button #
+        self.pushButton_2.clicked.connect(self.Selected)
 
         self.retranslateUi(Widget)
         QtCore.QMetaObject.connectSlotsByName(Widget)
@@ -155,25 +183,35 @@ class Ui_Widget(object):
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(_translate("Widget", "Widget", None))
         self.label_6.setText(_translate("Widget", "XMU WISE&SOE LECTURE ROBOT", None))
+        self.commandLinkButton_3.setText(_translate("Widget", "Fibears", None))
         self.label_2.setText(_translate("Widget", "PassWord", None))
         self.label.setText(_translate("Widget", "UserName", None))
         self.commandLinkButton.setText(_translate("Widget", "DropLink", None))
-        self.label_3.setText(_translate("Widget", "ClickMe...", None))
+        self.label_3.setText(_translate("Widget", "Step1.", None))
         self.commandLinkButton_2.setText(_translate("Widget", "MoreDetails", None))
         self.label_4.setText(_translate("Widget", "DropLink", None))
-        self.pushButton.setText(_translate("Widget", "Select...", None))
+        self.pushButton.setText(_translate("Widget", "GetCookie", None))
         self.label_5.setText(_translate("Widget", "MoreDetails", None))
         self.label_7.setText(_translate("Widget", "Author", None))
-        self.commandLinkButton_3.setText(_translate("Widget", "Fibears", None))
+        self.label_8.setText(_translate("Widget", "Step2.", None))
+        self.pushButton_2.setText(_translate("Widget", "GrabLecture", None))
+
+    def DropLink(self):
+        webbrowser.open('http://event.wisesoe.com/')
+
+    def MoreDetails(self):
+        webbrowser.open('http://fibears.top/2016/03/21/LectureRobot/')
+
+    def Author(self):
+        webbrowser.open('https://github.com/fibears')
 
     def GetCookies(self):
         self.textBrowser.append("Crawl the cookie......")
         print("Crawl the cookie......")
         # filename = 'cookie.txt'
         # cookie = cookielib.MozillaCookieJar(filename)
-        cookie = cookielib.MozillaCookieJar()
         # 利用urllib2库的HTTPCookieProcessor对象来创建cookie处理器
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookie))
         # Part1. 构建PostData，第一步输入Post的数据：用户名,密码和时间戳
         TimeStamp1 = int(time.time()*1000)
         TimeStamp1Data = urllib.urlencode({
@@ -210,27 +248,29 @@ class Ui_Widget(object):
         self.textBrowser.append("The cookie is saved on your computer!")
         self.textBrowser.append("Next step......")
         self.textBrowser.append("GrabLecture from website.")
+        self.textBrowser.append("------------------------------------")
+        self.textBrowser.append("Please Click GrabLecture Button!")
+        self.textBrowser.append("------------------------------------")
         print("The cookie is saved on your computer!")
         print("Next step =====> GrabLecture from website.")
-        return cookie
+        return self.cookie
 
 
     # Define the opener
     def getOpener(self):
         # Create MozillaCookieJar Object
-        #cookie = cookielib.MozillaCookieJar()
+        cookie = self.cookie
         # Load cookie.txt file
-        #cookie.load('cookie.txt', ignore_discard=True, ignore_expires=True)
-        self.cookie = self.GetCookies()
+        # cookie.load('cookie.txt', ignore_discard=True, ignore_expires=True)
         # Construct opener processor
-        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookie))
-        return self.opener
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+        return opener
 
     # Get the html parsed result
     def getParsed(self):
-        # opener = self.getOpener()
+        opener = self.getOpener()
         # Parse the response file by lxml.html.parse Method
-        parsed = parse(self.opener.open(self.LectureUrl))
+        parsed = parse(opener.open(self.LectureUrl))
         return parsed
 
     # Construct the PostData from parsed result
@@ -247,7 +287,9 @@ class Ui_Widget(object):
             self.textBrowser.append("Sorry!!!")
             self.textBrowser.append("No Seminar is active at present!")
             self.textBrowser.append("Bye-Bye!")
-            self.textBrowser.append("------Cutting Line------")
+            self.textBrowser.append("------------------------------------")
+            self.textBrowser.append("--------- Final Result ---------")
+            self.textBrowser.append("------------------------------------")
             print("Sorry!!!=======>No Seminar is active at present!")
             print("Bye-Bye!")
             print("-------------分割线------------")
@@ -280,8 +322,8 @@ class Ui_Widget(object):
     # Define Information Printing Function
     def PrintInformation(self):
         """Print SelectedLectures Information"""
-        # opener = self.getOpener()
-        NewResponse = self.opener.open(self.LectureUrl).read().decode('utf-8')
+        opener = self.getOpener()
+        NewResponse = opener.open(self.LectureUrl).read().decode('utf-8')
         parsed = lxml.html.fromstring(NewResponse)
         # Extract Information of Reserved Lectures
         SelectedLecture = parsed.xpath("//td/a[contains(@onclick, 'Cancel')]/../../td[2]/text()")
@@ -330,6 +372,9 @@ class Ui_Widget(object):
             print("Congratulation!!!You have reserved one seminar!!!")
         # Print information
         self.PrintInformation()
+        self.textBrowser.append("------------------------------------")
+        self.textBrowser.append("--------- Ending Line ---------")
+        self.textBrowser.append("------------------------------------")
 
 if __name__ == "__main__":
     import sys
